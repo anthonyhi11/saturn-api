061ecdcdb1c482670fb50585111415479840cd87
Anthony Hill <antdavhill@gmail.com>
2020-04-18 20:48:55 -0500

    made slight changes to login-router. added message to error.

--- src/login/login-router.js
+++ src/login/login-router.js
@@ -10,14 +10,14 @@ loginRouter.route("/").post(jsonBodyParser, (req, res, next) => {
   for (const [key, value] of Object.entries(loginUser))
     if (value == null) {
       return res.status(400).json({
-        error: `Missing '${key}' in request body`,
+        error: { message: `Missing '${key}' in request body` },
       });
     }
   LoginService.getUserWithEmail(req.app.get("db"), loginUser.email)
     .then((dbUser) => {
       if (!dbUser) {
         return res.status(400).json({
-          error: `Incorrect email or password`,
+          error: { message: `Incorrect email or password` },
         });
       } else {
         return LoginService.comparePasswords(
@@ -26,7 +26,7 @@ loginRouter.route("/").post(jsonBodyParser, (req, res, next) => {
         ).then((compareMatch) => {
           if (!compareMatch) {
             return res.status(400).json({
-              error: `Incorrect email or password`,
+              error: { message: `Incorrect email or password` },
             });
           }
           const sub = dbUser.email;
