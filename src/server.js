f0755cc730597dd46557a0b3e018c95d707e145f
Anthony Hill <antdavhill@gmail.com>
2021-04-26 17:21:36 -0500

    [SSL] Heroku changes to account for SSL
    - Since previous deployment, Heroku made a few changes to how the handle SSL. I had to include an AMC (auto managed cert) and upgrade pg to 8

--- src/server.js
+++ src/server.js
@@ -1,13 +1,12 @@
 const app = require("./app");
-const { PORT, DATABASE_URL } = require("./config");
+const { PORT, DATABASE_URL, NODE_ENV } = require("./config");
 const knex = require("knex");
 
 const db = knex({
   client: "pg",
-  connection: DATABASE_URL,
-  ssl: {
-    require: true,
-    rejectUnauthorized: false,
+  connection: {
+    connectionString : DATABASE_URL,
+    ssl: NODE_ENV == "development" ? false : { rejectUnauthorized: false }
   }
 });
 
